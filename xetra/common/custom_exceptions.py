d87df2740ec6d48d55f4878a8fd1fb43b95769f4
Ruoshi Wu <ruoshiwu1006@gmail.com>
2021-11-25 23:20:39 -0500

    11/25

--- xetra/common/custom_exceptions.py
+++ xetra/common/custom_exceptions.py
@@ -6,3 +6,8 @@ class WrongFormatException(Exception):
     WrongFormatException class
     Exception that can be raised when the format type given as param is not supported
     """
+
+class WrongMetaFileException(Exception):
+    """
+    WrongMetaFileException class
+    """
